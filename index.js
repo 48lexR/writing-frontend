1eb4cc512bd443a55ea114ccbb8183f430102cba
Cage Bullard <cbull358@gmail.com>
2026-06-06 18:57:07 -0400

    Index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const worksDiv = document.querySelector('.works')[0];
+
 const loadWorks = async () => {
-	const worksDiv = document.querySelector('.works')[0];	
-};
+	const rows = fetch('/works/');
+	const formatted = rows.forEach(row => {
+		return `<tr><td>${ row.title }</td><td>${ row.description }</td><td><small>Created at: ${ row.created_at }</small></td></tr>`
+	}).join('');
+	worksDiv.innerHTML = `<table><thead><th>Title</th><th>Description</th><th>Created</th></thead>${ formatted }</table>`;
+});
