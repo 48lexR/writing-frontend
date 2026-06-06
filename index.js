b3db2487f9e68eeed9a2c4a8f2b725c9141c4dad
Cage Bullard <cbull358@gmail.com>
2026-06-06 19:01:32 -0400

    Fixed

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const loadWorks = async () => {
 		return `<tr><td>${ row.title }</td><td>${ row.description }</td><td><small>Created at: ${ row.created_at }</small></td></tr>`
 	}).join('');
 	worksDiv.innerHTML = `<table><thead><th>Title</th><th>Description</th><th>Created</th></thead>${ formatted }</table>`;
-});
+};
